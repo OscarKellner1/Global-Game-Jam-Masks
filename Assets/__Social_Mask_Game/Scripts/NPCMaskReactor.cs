e11af28fc885c04d3842eb2976dbb7279fd614e1
AVataRR626 <matt.cabanag@gmail.com>
2026-01-27 16:46:22 +1100

    basic mechanics! :D

---
+++ Assets/__Social_Mask_Game/Scripts/NPCMaskReactor.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class NPCMaskReactor : MonoBehaviour
+{
+    public PlayerMaskManager subject;
+    public NMAWalkTowards walkingManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public UnityEvent onMaskA;
+    public UnityEvent onMaskB;
+    public UnityEvent onMaskC;
+    public UnityEvent onMaskD;
+    public UnityEvent onExit;
+
+    public bool onMaskFlag;
+    public int prevMaskType = -1;
+
+    public void Update()
+    {
+        if (subject != null)
+        {
+            if (prevMaskType != subject.maskType)
+                onMaskFlag = false;
+
+            if (!onMaskFlag)
+            {
+                Debug.Log(" pmm:" + subject.maskType);
+
+                if (subject.maskType == 0)
+                    onMaskA.Invoke();
+
+                if (subject.maskType == 1)
+                    onMaskB.Invoke();
+
+                if (subject.maskType == 2)
+                    onMaskC.Invoke();
+
+                if (subject.maskType == 3)
+                    onMaskD.Invoke();
+
+                onMaskFlag = true;
+            }
+
+            prevMaskType = subject.maskType;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("NPCMaskReactor: " + other.name);
+        subject = other.GetComponent<PlayerMaskManager>();
+        onMaskFlag = false;
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        subject = null;
+        onMaskFlag = false;
+        onExit.Invoke();
+    }
+
+    public void WalkToSubject()
+    {
+        if(subject != null)
+        {
+            walkingManager.destination = subject.transform;
+        }
+    }
+}
