9f74229bc056ea47c19ededadfa6399e0668bfc4
AVataRR626 <matt.cabanag@gmail.com>
2026-01-29 16:39:47 +1100

    sillouette randomisation

--- Assets/__Social_Mask_Game/Scripts/NPCManager.cs
+++ Assets/__Social_Mask_Game/Scripts/NPCManager.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class NPCManager : MonoBehaviour
 {
     public Color faceColour;
+    public ItemCycler billboardSelector;
+    public NPCBillboardManager myBillboardManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        billboardSelector.ActivateItemRandom();
+
+        myBillboardManager = billboardSelector.currentItem.GetComponent<NPCBillboardManager>();
     }
 
     // Update is called once per frame
