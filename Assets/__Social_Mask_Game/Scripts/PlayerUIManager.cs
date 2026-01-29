91f492cca1b71f3f265199fdfa4f04ccb73f07d5
AVataRR626 <matt.cabanag@gmail.com>
2026-01-29 13:57:02 +1100

    staring updates

--- Assets/__Social_Mask_Game/Scripts/PlayerUIManager.cs
+++ Assets/__Social_Mask_Game/Scripts/PlayerUIManager.cs
@@ -5,12 +5,17 @@ using UnityEngine.UI;
 
 public class PlayerUIManager : MonoBehaviour
 {
+    [Header("Player Link Settings")]
     public PlayerMaskManager myPlayerManager;
     public TextMeshProUGUI tmpTexDisplay;
     public string prefix = "Emo.Health: ";
     public Image healthBar;
+    public string healthDisplayString = "N0";
+    public Animator vignetteAnimator;
 
+    [Header("Events")]
     public UnityEvent onDamage;
+    public UnityEvent onDrain;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,8 +28,13 @@ public class PlayerUIManager : MonoBehaviour
     void Update()
     {
         if(tmpTexDisplay != null)
-            tmpTexDisplay.text = prefix + myPlayerManager.emotionalHealth.ToString();
+            tmpTexDisplay.text = prefix + myPlayerManager.emotionalHealth.ToString(healthDisplayString);
 
         healthBar.fillAmount = (myPlayerManager.emotionalHealth / 100);
+
+        if(vignetteAnimator != null)
+        {
+            vignetteAnimator.SetFloat("DPS", myPlayerManager.drainDamage);
+        }
     }
 }
